e0a180a15f4ff1c21cee44ad3e7e6a8f8ed57b6d
SyedRahim <ibrahimsyed624@gmail.com>
2019-04-08 23:20:50 +0530

    working with Go math/rand

--- Week1/First.go
+++ Week1/First.go
@@ -9,6 +9,7 @@ func main() {
 	var firstName = "syed"
 	var lastName = "ibrahim"
 	fmt.Println("This is my first program in Go using GoLand!!")
-	fmt.Println(rand.Intn(55))
+	fmt.Println(rand.Intn(100))
 	fmt.Println(firstName + " " + lastName)
+	fmt.Println(rand.Intn(1000))
 }
