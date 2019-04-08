2cb5f80336de3603646d5e02df7e37c9d291221c
unknown <ibrahimsyed624@gmail.com>
2019-04-08 23:00:27 +0530

    Initial commit for my Golang repo

---
+++ Week1/First.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	var firstName = "syed"
+	var lastName = "ibrahim"
+	fmt.Println("This is my first program in Go using GoLand!!")
+	fmt.Println(rand.Intn(55))
+	fmt.Println(firstName + " " + lastName)
+}
