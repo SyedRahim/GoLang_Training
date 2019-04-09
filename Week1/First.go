ce9d6fee635b7e4f12209803bfed2e231cdf4617
SyedRahim <ibrahimsyed624@gmail.com>
2019-04-09 23:38:54 +0530

    Working with for loop and if else in GoLang

--- Week1/First.go
+++ Week1/First.go
@@ -12,4 +12,11 @@ func main() {
 	fmt.Println(rand.Intn(100))
 	fmt.Println(firstName + " " + lastName)
 	fmt.Println(rand.Intn(1000))
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%d is even number\n", i)
+		} else {
+			fmt.Printf("%d is odd number\n", i)
+		}
+	}
 }
